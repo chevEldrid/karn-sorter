033c6e03f9d2a225001a01baef80ac4b15f9397c
Chev <chev424@gmail.com>
2019-05-24 11:07:02 -0400

    warnings added before clearing directories

--- clear_lists.py
+++ clear_lists.py
@@ -3,11 +3,17 @@ import os
 card_lists = ["cards.txt", "prices.txt"]
 PRG_PATH = "/home/pi/karn-sorter"
 FOLDER = "test"
-#clears cards.txt and prices.txt for testing purposes
-for l in card_lists:
-    with open(l, "w") as myfile:
-        myfile.write("")
-print "storage files cleared!"
-for filename in os.listdir(PRG_PATH+'/'+FOLDER):
-    os.remove(filename)
-print "card pictures cleared!"
+
+input = raw_input("This script will clear all stored card lists AND delete all card photos in " + PRG_PATH+"/"+FOLDER+"...type [y]es to continue:  ")
+
+if input == "y":
+    #clears cards.txt and prices.txt for testing purposes
+    for l in card_lists:
+        with open(l, "w") as myfile:
+            myfile.write("")
+    print "storage files cleared!"
+    for filename in os.listdir(PRG_PATH+'/'+FOLDER):
+        os.remove(FOLDER + "/" + filename)
+    print "card pictures cleared from local directory!"
+else:
+    print "input not recognized. try again!"
