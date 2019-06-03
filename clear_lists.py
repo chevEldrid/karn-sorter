38d798ddc61d80cdd7f01cb7c09a091058c4423c
Chev <chev424@gmail.com>
2019-06-03 10:39:52 -0400

    naming organization and adding crop clearing to clear

--- clear_lists.py
+++ clear_lists.py
@@ -3,6 +3,7 @@ import os
 card_lists = ["cards.txt", "prices.txt"]
 PRG_PATH = "/home/pi/karn-sorter"
 FOLDER = "test"
+CP_FOLDER = "crops"
 
 input = raw_input("This script will clear all stored card lists AND delete all card photos in " + PRG_PATH+"/"+FOLDER+"...type [y]es to continue:  ")
 
@@ -15,5 +16,9 @@ if input == "y":
     for filename in os.listdir(PRG_PATH+'/'+FOLDER):
         os.remove(FOLDER + "/" + filename)
     print "card pictures cleared from local directory!"
+    #removes any leftovers from the crops directory (if you ran cam_setup.py)
+    for filename in os.listdir(PRG_PATH+'/'+CP_FOLDER):
+        os.remove(CP_FOLDER + "/" + filename)
+    print "crop tests removed from /"+CP_FOLDER
 else:
     print "input not recognized. try again!"
